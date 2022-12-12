868f14a9c21de202a51db7ea4c112bf829c5c145
Stan Gilstrap <stan.gilstrap@ahead.com>
2022-12-12 08:48:29 -0500

    Initial Build

---
+++ WebAdver.Web/Controllers/Accounts.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdver.Web.Controllers
+{
+    public class Accounts : Controller
+    {
+        // GET: Accounts
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
